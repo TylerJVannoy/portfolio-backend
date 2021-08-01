912505e29dded16f2f09b25a7692dd35686b5641
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-08-01 09:23:52 -0700

    cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 
