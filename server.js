0154fc8c6daf34800f3f3887748e54ba70ffee20
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-08-01 09:38:10 -0700

    added bodyParser

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json());
